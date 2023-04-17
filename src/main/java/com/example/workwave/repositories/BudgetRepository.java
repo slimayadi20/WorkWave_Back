57b3cfe1ddd2357a7c17e7955933614abcc7f8c6
Wassim Ghiloufi <48228694+Ghiloufi1998@users.noreply.github.com>
2023-04-17 02:38:52 +0100

    fix budgetRepository

--- src/main/java/com/example/workwave/repositories/BudgetRepository.java
+++ src/main/java/com/example/workwave/repositories/BudgetRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface BudgetRepository extends JpaRepository<Budget,Long> {
 
-    List<Budget> findByUser(User user);
-
 }
