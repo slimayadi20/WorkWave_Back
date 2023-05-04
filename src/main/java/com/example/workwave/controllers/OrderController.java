78c8224ac20b22065ab27d327da9d6e49626349c
Wassim Ghiloufi <ghiloufiwassim@gmail.com>
2023-05-04 21:44:53 +0100

    merge with aziz first commit

---
+++ src/main/java/com/example/workwave/controllers/OrderController.java
@@ -0,0 +1,62 @@
+package com.example.workwave.controllers;
+
+import com.example.workwave.entities.Order;
+import com.example.workwave.entities.products;
+import com.example.workwave.repositories.OrderRepository;
+import com.example.workwave.services.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    OrderRepository OrderRepository;
+
+    @Autowired
+    OrderServiceImpl Orderservice;
+
+    @PostMapping("/addOrder")
+    public String addOrder(@RequestBody Order order  ) {
+        Orderservice.addOrder(order);
+        Set<products> products = order.getProduct();
+        if (products != null && !products.isEmpty()) {
+            products product = products.iterator().next();
+            long p_id = product.getP_id();
+            // do something with p_id
+            Orderservice.addOrderToProduct(order.getO_id() , p_id);
+        }
+
+
+
+        return  "ok";
+
+    }
+    @DeleteMapping("/deleteOrder/{o_id}")
+    public String deleteOrder(@PathVariable long o_id) {
+
+        return Orderservice.deleteOrder(o_id);
+    }
+    @PutMapping("/updateOrder")
+    public Order updateOrder(@RequestBody Order Order) {
+
+        return Orderservice.updateOrder(Order);
+    }
+    @GetMapping("/Order")//affichage+pagination
+    public List<Order> show() {
+        return Orderservice.GetAllOrder();
+
+    }
+
+
+
+
+
+
+
+
+}
